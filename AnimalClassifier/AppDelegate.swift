f7385d0ddd67e99475edb6d216cdb88ca68ddd80
sindhya <sindhyaanoop@hotmail.com>
2024-11-11 17:02:53 -0500

    Initial Commit

---
+++ AnimalClassifier/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AnimalClassifier
+//
+//  Created by SINDHYA ANOOP on 7/19/21.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AnimalClassifier/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AnimalClassifier
+//
+//  Created by SINDHYA ANOOP on 7/19/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
